b6ae7ae5797b56fe4675fe2f150ea01a0de8dfea
Nathan <nathanael.esnault@gmail.com>
2020-02-11 18:54:33 +1300

    Update CORS M8T.bat

--- CORS M8T.bat
+++ CORS M8T.bat
@@ -2,10 +2,10 @@
 
 CORSFinished=false
 
-# this function is called when Ctrl-C is sent
+# This function is called when Ctrl-C is sent
 trap_ctrlc()
 {
-	if [ "$CORSFinished" = false ] ; then #continue the rest of the program
+	if [ "$CORSFinished" = false ] ; then # Continue the rest of the program
 		# perform cleanup here
 		echo "Ctrl-C caught...performing clean up"
 
@@ -17,15 +17,15 @@ trap_ctrlc()
 		cd /home/pi/RTKLIB/app/str2str/gcc
 		
 		#Ask User for coordinates of BS
-		read -p "Waiting for Latitude: " var1
+		read -p "Waiting for Latitude (1/3): " var1
 
-		read -p "Waiting for input: " var2
+		read -p "Waiting for Longitude (2/3): " var2
 
-		read -p "Waiting for input: " var3
+		read -p "Waiting for Altitude (3/3): " var3
 
 		# Stream the RTCM3 content with the following messages
-		# 1005,1006,1008,1033,1074,1013,1084,1094,1114,1124 (As AUCK_RTCM-MSM)
-		# Station ID 445
+		# 1005,1006,1008,1033,1074,1013,1084,1094,1114,1124 (as AUCK_RTCM-MSM)
+		# Station is chosen as ID 445
 		sudo ./str2str -in serial://ttyACM0:230400:8:n:1:off#ubx -out serial://ttyS0:57600:8:n:1:off#rtcm3 -msg 1005,1006,1008,1033,1074,1013,1084,1094,1114,1124 -p 52.14271937 3.24461 5.6753
 		
 		#In case of Ctrl-C, indicates that the CORS is already done
@@ -35,16 +35,16 @@ trap_ctrlc()
 		echo 'End of Script'
 	fi	
 	
-	# exit shell script with error code 2
+	# Exit shell script with error code 2,
 	# if omitted, shell script will continue execution
 	exit 2
 }
 
-# initialise trap to call trap_ctrlc function
+# initialise trap to call "trap_ctrlc" function
 # when signal 2 (SIGINT) is received
 trap "trap_ctrlc" 2
 
-## This is the passthrough: RPi is a glorified Serial2WLAN converter
+# This is the passthrough: RPi is a glorified Serial2Wifi converter
 #grab Ucenter and perform a Receiver -->  Port --> Network Connection --> New or select
 
 while true; do
